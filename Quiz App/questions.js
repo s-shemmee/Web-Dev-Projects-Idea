cc3a6b2162d9b81632009b293e50b4ece27b0f67
s-sheemee <56132945+s-shemmee@users.noreply.github.com>
2022-10-26 20:58:41 +0100

    Update

---
+++ Quiz App/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is a superclass of every class in Java?",
+    answer: "Object class",
+    options: [
+      "ArrayList",
+      "Abstract class",
+      "Object class",
+      "String"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of these are selection statements in Java?",
+    answer: "If()",
+    options: [
+      "Break",
+      "Continue",
+      "For()",
+      "If()"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the extension of compiled java classes?",
+    answer: ".class",
+    options: [
+      ".class",
+      ".txt",
+      ".js",
+      ".java"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which exception is thrown when java is out of memory?",
+    answer: "MemoryPutOfBoundsException",
+    options: [
+      "MemoryError",
+      "OutOfMemoryError",
+      "MemoryFullException",
+      "MemoryPutOfBoundsException"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the below is not a Java Profiler?",
+    answer: "JVM",
+    options: [
+      "Jprofiler",
+      "JVM",
+      "Eclipse Profiler",
+      "JConsole"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which concept of Java is achieved by combining methods and attribute into a class?",
+    answer: "Encapsulation",
+    options: [
+      "Encapsulation",
+      "Inheritance",
+      "Polymorphism",
+      "Abstraction"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of these cannot be used for a variable name in Java?",
+    answer: "Keyword",
+    options: [
+      "Identifier & Keyword",
+      "Identifier",
+      "Keyword",
+      "None of the mentioned"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Number of primitive data types in Java are?",
+    answer: "8",
+    options: [
+      "4",
+      "2",
+      "8",
+      "6"
+    ]
+  },
+  {
+    numb: 9,
+    question: "correct extension of the Python file?",
+    answer: ".java",
+    options: [
+      ".py",
+      ".java",
+      ".c",
+      ".python"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which component is used to compile, debug and execute the java programs?",
+    answer: "JDK",
+    options: [
+      "JRE",
+      "Jit",
+      "JDK",
+      "JVM"
+    ]
+  },
+];
